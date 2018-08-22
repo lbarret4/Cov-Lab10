465a5cda75cb205f74758a9908cbbe776a747764
lbarret4 <llewellynbar4@gmail.com>
2018-08-22 01:13:49 -0400

    Modified createSong to return array and remove h,p from index

--- 99linesofcode.js
+++ 99linesofcode.js
@@ -7,18 +7,19 @@ function printSong(names){
 }
 
 function createSong(name) {
+    let lyrics = [];
     for (i = 99; i > 0; i--) {
         switch (i) {
             case 1:
-                console.log(`${i} lines of code in the file, ${i} lines of code; ${name} strikes one out, clears it all out, no more lines of code in the file`);
+                lyrics.push(`${i} lines of code in the file, ${i} lines of code; ${name} strikes one out, clears it all out, no more lines of code in the file`);
                 break;
             case 99:
-                console.log(`${name}:`);
+                lyrics.push(`${name}:`);
             default:
-                console.log(i + ' lines of code in the file,' + i + ` lines of code; ${name} strikes one out, clears it all out, ${i - 1} lines of code in the file`);
+                lyrics.push(i + ' lines of code in the file,' + i + ` lines of code; ${name} strikes one out, clears it all out, ${i - 1} lines of code in the file`);
         }
 
 
     }
-
+    return lyrics;
 }
